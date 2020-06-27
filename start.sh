645a92c63bff4d5912ee3fe59309562b6e086402
ander002 <36130880+ander002@users.noreply.github.com>
2020-06-27 10:12:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 /vhs/kangle/bin/kangle
 service mysqld start
 
- echo "安装成功，默认用户名admin，密码kangle，浏览器打开：http://ip:3311"    
+ echo "安装成功，默认用户名admin，密码kangle，浏览器打开：http://ip:3312/admin"    
 exec /usr/sbin/sshd -D
