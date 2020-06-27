5eb7ae1e130ec73cc657042268ec99e1fcb6b8f6
ander002 <209875203@qq.com>
2020-06-26 22:35:16 -0400

    修改了输出文件，更加简单易操作

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 /vhs/kangle/bin/kangle
 service mysqld start
 
- echo "安装成功，默认用户名admin，密码kangle，浏览器打开：http://ip:3311"    
+ echo "安装成功，默认用户名admin，密码kangle，MySQL 用户名root 密码为空 记得修改后修改服务器配置 浏览器打开：http://ip:3312/admin  配置服务器，"    
 exec /usr/sbin/sshd -D
